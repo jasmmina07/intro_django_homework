94f92561effd8fdc75a3fcb9f4aaf6756ce707b7
jasminatarmasheva786 <jasminatarmasheva786@gmail.com>
2024-02-12 21:07:54 +0500

    Done

--- api/views.py
+++ api/views.py
@@ -4,11 +4,13 @@ data = {
     1: {
         'name': 'Milk',
         'price': 2.5,
+        "type":"Milk",
         'quantity': 10
     },
     2: {
         'name': 'Bread',
         'price': 1.5,
+        'type':'Bread',
         'quantity': 20
     },
 }
@@ -19,8 +21,38 @@ data = {
 # GET /grocery/name/<name> - this endpoint will display a list of fruits by specifying the fruit name in the URL.
 # GET /grocery/price/<price> - this endpoint will display a list of fruits by specifying the fruit price in the URL.
 
+def home(request: HttpRequest):
+    return JsonResponse({"message":"Welcome!"})
+
 def get_all_items(request: HttpRequest):
     return JsonResponse(data)
+
+def grocery_by_type(request: HttpRequest,type):
+    resp=[]
+    for i in data:
+        if data[i]["type"]==type:
+            resp.append(data[i])
+    return JsonResponse({"response":resp})
+
+def grocery_by_price(request: HttpRequest,price):
+    resp=[]
+    for i in data:
+        if data[i]["price"]<=int(price):
+            resp.append(data[i])
+    return JsonResponse({"response":resp})
+
+def grocery_by_name(request: HttpRequest,name):
+    resp=[]
+    for i in data:
+        if data[i]["name"]==name:
+            resp.append(data[i])
+    return JsonResponse({"response":resp})
+
+def grocery(request: HttpRequest):
+    arr=[]
+    for i in data:
+        arr.append(data[i]["name"])
+    return JsonResponse({"products":arr})
 
 def add_item(request: HttpRequest):
     if request.method=='POST':
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,21 @@
 # from django.contrib import admin
 from django.urls import path
-from api.views import get_all_items,add_item
+from api.views import (
+                       get_all_items,
+                       add_item,
+                       home,
+                       grocery,
+                       grocery_by_type,
+                       grocery_by_name,
+                       grocery_by_price
+                       )
 
 urlpatterns = [
     path('items/',get_all_items),
-    path('additem/',add_item)
+    path('additem/',add_item),
+    path("",home),
+    path("grocery/",grocery),
+    path("grocery/type/<type>",grocery_by_type),
+    path("grocery/name/<name>",grocery_by_name),
+    path("grocery/price/<price>",grocery_by_price)
 ]
